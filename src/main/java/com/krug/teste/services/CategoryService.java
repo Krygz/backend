52b25acfbb14cb37e42037759d333cf041177249
Pedro <ph4290245@gmail.com>
2024-01-12 14:14:42 -0300

    DTO

--- src/main/java/com/krug/teste/services/CategoryService.java
+++ src/main/java/com/krug/teste/services/CategoryService.java
@@ -1,20 +1,24 @@
 package com.krug.teste.services;
 
+import com.krug.teste.dto.CategoryDTO;
 import com.krug.teste.model.Category;
 import com.krug.teste.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 @Transactional(readOnly = true)
-    public List<Category> findAll(){
-        return categoryRepository.findAll();
+    public List<CategoryDTO> findAll(){
+        List<Category> list = categoryRepository.findAll();
+        return list.stream().map(x -> new CategoryDTO(x)).toList();
     }
 
 }
