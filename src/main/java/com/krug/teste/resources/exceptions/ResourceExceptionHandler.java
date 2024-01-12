3268e13b617ed1118d11b510ef6336f56064720f
Pedro <ph4290245@gmail.com>
2024-01-12 17:03:34 -0300

    Find Category by id and ExceptionHandler

---
+++ src/main/java/com/krug/teste/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.krug.teste.resources.exceptions;
+
+import com.krug.teste.services.exceptions.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+import java.util.Locale;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError>  entityNotFound(EntityNotFoundException e , HttpServletRequest request){
+      StandardError error = new StandardError();
+      error.setTimeStamp(Instant.now());
+      error.setStatus(HttpStatus.NOT_FOUND.value());
+      error.setError("Resource not found");
+      error.setMessage(e.getMessage());
+      error.setPath(request.getRequestURI());
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+}
--- src/main/java/com/krug/teste/services/CategoryService.java
+++ src/main/java/com/krug/teste/services/CategoryService.java
@@ -3,12 +3,16 @@ package com.krug.teste.services;
 import com.krug.teste.dto.CategoryDTO;
 import com.krug.teste.model.Category;
 import com.krug.teste.repositories.CategoryRepository;
+import com.krug.teste.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -20,5 +24,11 @@ public class CategoryService {
         List<Category> list = categoryRepository.findAll();
         return list.stream().map(x -> new CategoryDTO(x)).toList();
     }
+@Transactional(readOnly = true)
+    public CategoryDTO findById(Long id){
+        Optional<Category> obj  = categoryRepository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
 
+    }
 }
