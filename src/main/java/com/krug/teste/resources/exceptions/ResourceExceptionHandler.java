92f4546ecc07994002ed29aa2d3a7001cc97e7d8
Pedro <ph4290245@gmail.com>
2024-01-21 01:41:42 -0300

    Custom validation response

--- src/main/java/com/krug/teste/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/krug/teste/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,8 @@ import com.krug.teste.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,5 +39,21 @@ public class ResourceExceptionHandler {
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
       return  new ResponseEntity<StandardError>(err ,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<ValidationError> notValidArgument(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Not Valid Argument");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+       for(FieldError f : e.getBindingResult().getFieldErrors()){
+              err.addError(f.getField() , f.getDefaultMessage());
+
+       }
+        return  new ResponseEntity<ValidationError>(err ,HttpStatus.UNPROCESSABLE_ENTITY);
     }
 }
